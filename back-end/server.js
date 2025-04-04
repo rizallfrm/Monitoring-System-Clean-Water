30593f65797bba5187a6f1b0b58719bdb7e527fe
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2025-04-04 17:33:57 +0700

    feat: add packages and set server.js

---
+++ back-end/server.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const userService = require('./services/user-services/app');
+const reportService = require('./services/report-service/app');
+const statusService = require('./services/status-service/app');
+const actionService = require('./services/action-service/app');
+
+// Port untuk masing-masing service
+const USER_SERVICE_PORT = process.env.USER_SERVICE_PORT || 3001;
+const REPORT_SERVICE_PORT = process.env.REPORT_SERVICE_PORT || 3002;
+const STATUS_SERVICE_PORT = process.env.STATUS_SERVICE_PORT || 3003;
+const ACTION_SERVICE_PORT = process.env.ACTION_SERVICE_PORT || 3004;
+
+// Jalankan semua service
+userService.listen(USER_SERVICE_PORT, async () => {
+  console.log(`User Service berjalan di port ${USER_SERVICE_PORT}`);
+});
+
+reportService.listen(REPORT_SERVICE_PORT, async () => {
+  console.log(`Report Service berjalan di port ${REPORT_SERVICE_PORT}`);
+});
+
+statusService.listen(STATUS_SERVICE_PORT, async () => {
+  console.log(`Status Service berjalan di port ${STATUS_SERVICE_PORT}`);
+});
+
+actionService.listen(ACTION_SERVICE_PORT, async () => {
+  console.log(`Action Service berjalan di port ${ACTION_SERVICE_PORT}`);
+});
+
+console.log('Semua microservice telah dijalankan!');
+console.log(`
+Layanan yang tersedia:
+- User Service: http://localhost:${USER_SERVICE_PORT}
+- Report Service: http://localhost:${REPORT_SERVICE_PORT}
+- Status Service: http://localhost:${STATUS_SERVICE_PORT}
+- Action Service: http://localhost:${ACTION_SERVICE_PORT}
+`);
