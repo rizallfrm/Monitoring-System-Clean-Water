8ed80c54605d0da55f140e9d054bb4ad1811b091
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2025-04-29 21:04:01 +0700

    feat: add userService to handle integrate API

---
+++ front-end/src/services/userService.js
@@ -0,0 +1,49 @@
+// src/services/userService.js
+import api from './apiService';
+
+export const userService = {
+  getAllUsers: async () => {
+    try {
+      const response = await api.get('/users');
+      return response.data;
+    } catch (error) {
+      throw error.response?.data || error.message;
+    }
+  },
+
+  getUserById: async (id) => {
+    try {
+      const response = await api.get(`/users/${id}`);
+      return response.data;
+    } catch (error) {
+      throw error.response?.data || error.message;
+    }
+  },
+
+  updateUser: async (id, userData) => {
+    try {
+      const response = await api.put(`/users/${id}`, userData);
+      return response.data;
+    } catch (error) {
+      throw error.response?.data || error.message;
+    }
+  },
+
+  deleteUser: async (id) => {
+    try {
+      const response = await api.delete(`/users/${id}`);
+      return response.data;
+    } catch (error) {
+      throw error.response?.data || error.message;
+    }
+  },
+
+  getOfficers: async () => {
+    try {
+      const response = await api.get('/officers');
+      return response.data;
+    } catch (error) {
+      throw error.response?.data || error.message;
+    }
+  },
+};
