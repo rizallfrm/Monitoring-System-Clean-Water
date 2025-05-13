d1634c47ced4dd58d749b42837358a82c5e0fa6f
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2025-05-13 21:03:19 +0700

    update: modify apiService to handle service api

--- front-end/src/services/apiService.js
+++ front-end/src/services/apiService.js
@@ -1,34 +1,52 @@
 import axios from "axios";
 
-const API_BASE_URL = "hhtp://localhost:3000/api";
+const servicePorts = {
+  user: "http://localhost:3001/api",
+  report: "http://localhost:3002/api",
+  status: "http://localhost:3003/api",
+  action: "http://localhost:3004/api",
+};
 
-const api = axios.create({
-  baseURL: API_BASE_URL,
-});
+export const api = (serviceName) => {
+  const instance = axios.create({
+    baseURL: servicePorts[serviceName],
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+  });
+  instance.defaults.headers = instance.defaults.headers || {};
+  instance.defaults.headers.common = instance.defaults.headers.common || {};
+  // Interceptor request: tambahkan token
+  instance.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => Promise.reject(error)
+  );
 
-// Interceptor untuk menambahkan token ke header
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+  // Interceptor response: tangani 401
+  instance.interceptors.response.use(
+    (response) => {
+      return {
+        data: response.data,
+        status: response.status,
+        headers: response.headers,
+        config: response.config,
+      };
+    },
+    (error) => {
+      if (error.response?.status === 401) {
+        localStorage.removeItem("token");
+        window.location.href = "/login";
+      }
+      return Promise.reject(error);
     }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+  );
 
-
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem("token");
-      window.location.href = "/login";
-    }
-    return Promise.reject(error);
-  }
-);
-export default api;
+  return instance;
+};
