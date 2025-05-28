bd557e1d5e39ed01d1a5f2b4326437487771fd97
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2025-05-28 22:54:21 +0700

    update: modify report migrations to handle upload images

--- back-end/migrations/20250404034337-create-report.js
+++ back-end/migrations/20250404034337-create-report.js
@@ -1,61 +1,67 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('reports', {
+    await queryInterface.createTable("reports", {
       report_id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       user_id: {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'users',
-          key: 'user_id'
+          model: "users",
+          key: "user_id",
         },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE'
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
       },
       description: {
         type: Sequelize.TEXT,
-        allowNull: false
+        allowNull: false,
       },
       location: {
         type: Sequelize.STRING(255),
-        allowNull: false
-      },
-      status: {
-        type: Sequelize.ENUM('Cancel', 'Pending', 'On-Going', 'Completed'),
         allowNull: false,
-        defaultValue: 'Pending'
+      },
+       images: {
+        type: Sequelize.JSON,
+        allowNull: true,
+        defaultValue: [],
+      },
+
+      status: {
+        type: Sequelize.ENUM("Cancel", "Pending", "On-Going", "Completed"),
+        allowNull: false,
+        defaultValue: "Pending",
       },
       assigned_to: {
         type: Sequelize.INTEGER,
         allowNull: true,
         references: {
-          model: 'users',
-          key: 'user_id'
+          model: "users",
+          key: "user_id",
         },
-        onUpdate: 'CASCADE',
-        onDelete: 'SET NULL'
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL",
       },
       created_at: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      }
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
     });
-    
+
     // Menambahkan indeks untuk kolom yang sering dicari
-    await queryInterface.addIndex('reports', ['user_id']);
-    await queryInterface.addIndex('reports', ['assigned_to']);
-    await queryInterface.addIndex('reports', ['status']);
+    await queryInterface.addIndex("reports", ["user_id"]);
+    await queryInterface.addIndex("reports", ["assigned_to"]);
+    await queryInterface.addIndex("reports", ["status"]);
   },
-  
+
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('reports');
-  }
+    await queryInterface.dropTable("reports");
+  },
 };
